831f6866c7b7b2c7ee2f0351b3ce9421e9811333
Kpeng <294336370@qq.com>
2017-11-01 23:28:47 +0800

    五角星 滑动没有实现

--- MainProject/VC/SimpleViewController.m
+++ MainProject/VC/SimpleViewController.m
@@ -7,7 +7,7 @@
 //
 
 #import "SimpleViewController.h"
-
+#import "KpengScoreForJudge.h"
 @interface SimpleViewController ()<UITableViewDelegate,UITableViewDataSource>
 @property (nonatomic,strong) NSArray *dataArr;
 @end
@@ -20,6 +20,11 @@
     _tableView.dataSource =self;
 
     _dataArr =@[@"网易严选竖着",@"网易严选横着",@"星星评分控件",@"点赞收藏控件",@"自定义键盘",@"判断点是否在某个区域内",@"上下翻转的banner广告含点击事件"];
+    
+    KpengScoreForJudge *judge =[[KpengScoreForJudge alloc] initWithFrame:CGRectMake(100, CGRectGetMaxY(self.view.frame)-100, 300, 100)];
+    judge.backgroundColor =[UIColor redColor];
+    judge.needMoveTouch =YES;
+    [self.view addSubview:judge];
     
 }
 
